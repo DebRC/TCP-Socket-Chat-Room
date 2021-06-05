71306c305a73cb2a5834f2ab74fd763adf42c461
DebRC <roychowdhurydebdoot@gmail.com>
2021-06-05 19:46:57 +0530

    Finished client app

--- client.py
+++ client.py
@@ -1,1 +1,48 @@
 import socket, threading
+
+
+'''Global Variables'''
+# port number
+PORT = 5068
+# server ip
+SERVER = "127.0.1.1"
+# socket address ip+port
+ADDR = (SERVER, PORT)
+# specifies the max length of the message
+HEADER = 1024
+# format for encoding/decoding
+FORMAT = "utf-8"
+# message to disconnect
+DISCONNECT = "exit"
+
+
+'''Socket Object'''
+# creating a new client socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connecting the client object with server's socket
+client.connect(ADDR)
+
+# function for sending one message
+def send(msg):
+    # encode the message
+    message = msg.encode(FORMAT)
+    # pad extra bits to maintain HEADER size
+    msg_length = str(len(message)).encode(FORMAT)
+    msg_length += b' ' * (HEADER - len(msg_length))
+    # send message length and message
+    client.send(msg_length)
+    client.send(message)
+    # printing "message" coming from
+    # server side
+    print(client.recv(HEADER).decode(FORMAT))
+
+
+def start_client():
+    print("Client-server connection is ready! Type 'exit' to disconnect")
+    while True:
+        msg=input("Type Your Message - ")
+        send(msg)
+        if msg==DISCONNECT:
+            exit()
+
+start_client()
